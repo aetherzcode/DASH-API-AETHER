d1a57b12dfa9847f180bce5e97cc075374a704f8
FuadXyro <fuadxy99@gmail.com>
2024-09-29 23:02:48 +0700

    FuadXyro

---
+++ controllers/settings.js
@@ -0,0 +1,13 @@
+global.author = "FuadXyro";
+global.key_free = ["zenf"];
+global.key_premium = ['zenkey'];
+global.limit_free = "100";
+global.limit_premium = "99999";
+global.dateLimit = "Oktober, 31 2050",
+global.mongo_Db = "mongodb+srv://zeltoria1909:96xAwEIGoHhOO2lR@cluster0.v5c9etv.mongodb.net/?retryWrites=true&w=majority";
+global.recaptcha_key_1 = "6LegHYwmAAAAAOmYT82OldAD5MOpo7S9g5pT0XfQ";
+global.recaptcha_key_2 = "6LegHYwmAAAAAJSGEwz5G7jCD4tbBvrT-8nx3mBV";
+global.my_email = "fuadbotzmd@gmail.com";
+global.my_email_password = "aaxypvmdclsjpvgu";
+global.activation_token = "-@Pqnap+@(/1jAPPnew/@10";
+global.port = 8080
